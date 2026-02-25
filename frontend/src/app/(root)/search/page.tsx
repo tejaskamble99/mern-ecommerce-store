b2ce4320342ac9a93b6dd3851a81b6ec3126d829
Tejas Kamble <70898998+tejaskamble99@users.noreply.github.com>
2026-02-25 17:03:51 +0530

    searchpage created

--- frontend/src/app/(root)/search/page.tsx
+++ frontend/src/app/(root)/search/page.tsx
@@ -1,10 +1,98 @@
+"use client";
+import { useState } from "react";
+import ProductCard from "./../../../components/layout/ProductCard";
 
+export default function Search() {
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
 
-export default function DashboardPage() {
+  const addToCartHandler = () => {
+    console.log("Added to cart");
+  };
+  const handleSearch = () => {};
+
+  const isPrevPage = true;
+  const isNextPage = true;
+
   return (
-    <div className="min-h-screen p-8">
-      <h1 className="text-3xl font-bold mb-6">DashBoard</h1>
-      
+    <div className="search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low )</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price : {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={10000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="asc">Sample 1</option>
+            <option value="dsc">sample 2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div>
+          <ProductCard
+            productId="qedcsd"
+            name="Logitech MX Master 3S"
+            price={9500}
+            stock={1}
+            photo={
+              "https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/mice/mx-master-3s/gallery/mx-master-3s-mouse-top-view-graphite.png?v=1"
+            }
+            handler={addToCartHandler}
+          />
+        </div>
+
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} To {4}
+          </span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </article>
+      </main>
     </div>
   );
 }
