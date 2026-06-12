0b9caabef7bfef8626b9f9ad1ec525b5fba6b02a
Tejas Kamble <70898998+tejaskamble99@users.noreply.github.com>
2026-06-12 16:22:46 +0530

    fix: remove pathname restriction for onrender images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -20,7 +20,6 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "mern-ecommerce-store-6u04.onrender.com",
-        pathname: "/uploads/**",
       },
     ],
   },
